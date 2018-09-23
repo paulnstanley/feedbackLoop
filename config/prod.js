64c2ad4b81e63dbe7d92fe8c6d51a48b577e9cee
shodan993 <paulnstanley@gmail.com>
2018-09-23 16:28:25 -0400

    updating prod keys again

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
-  googleClientID: process.env.'425873972634-gcmf2jm8iqvs77us8iti69ste5ntdfr0.apps.googleusercontent.com',
-  googleClientSecret: process.env.'wOmAas9GlXdqaAFK1qmlzW3g',
-  mongoURI: process.env.`mongodb://heroku_znjv34dk:6cjkm8ihfhu0ks5to01ng0q4aa@ds111063.mlab.com:11063/heroku_znjv34dk`,
-  cookieKey: process.env.'sdlkfj8d092k392nlkns'
+  googleClientID: '425873972634-gcmf2jm8iqvs77us8iti69ste5ntdfr0.apps.googleusercontent.com',
+  googleClientSecret: 'wOmAas9GlXdqaAFK1qmlzW3g',
+  mongoURI: `mongodb://heroku_znjv34dk:6cjkm8ihfhu0ks5to01ng0q4aa@ds111063.mlab.com:11063/heroku_znjv34dk`,
+  cookieKey: 'sdlkfj8d092k392nlkns'
 };
