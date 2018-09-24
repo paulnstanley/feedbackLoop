b1f2a470b575d23de11d4f9d3dd38076f0f4fb26
shodan993 <paulnstanley@gmail.com>
2018-09-24 10:21:18 -0400

    changing prod.js to get vars from heroku instead of hard-coding

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
-  googleClientID: '425873972634-gcmf2jm8iqvs77us8iti69ste5ntdfr0.apps.googleusercontent.com',
-  googleClientSecret: 'wOmAas9GlXdqaAFK1qmlzW3g',
-  mongoURI: `mongodb://heroku_znjv34dk:6cjkm8ihfhu0ks5to01ng0q4aa@ds111063.mlab.com:11063/heroku_znjv34dk`,
-  cookieKey: 'sdlkfj8d092k392nlkns'
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
 };
