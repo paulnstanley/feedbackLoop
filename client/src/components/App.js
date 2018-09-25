0197119a1ab7dcd8cf149d7f531fa9ae292a6e01
shodan993 <paulnstanley@gmail.com>
2018-09-25 10:32:57 -0400

    added survey forms, payment processing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,8 +4,8 @@ import { connect } from 'react-redux';
 import * as actions from '../actions';
 
 import Header from './Header';
-const Dashboard = () => <h2>Dashboard</h2>;
-const SurveyNew = () => <h2>SurveyNew</h2>;
+import Dashboard from './Dashboard';
+import SurveyNew from './surveys/SurveyNew';
 const Landing = () => <h2>Landing</h2>;
 
 class App extends Component {
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Payments from './Payments';
 
 class Header extends Component {
   renderContent() {
@@ -7,9 +9,15 @@ class Header extends Component {
       case null:
         return;
       case false:
-        return <li><a href="/auth/google">Log in with Google</a></li>;
+        return <li><a href="/auth/google">Login With Google</a></li>;
       default:
-        return <li><a href="/api/logout">Logout</a></li>;
+      return [
+        <li key="1"><Payments /></li>,
+        <li key="3" style={{ margin: '0 10px' }}>
+          Credits: {this.props.auth.credits}
+        </li>,
+        <li key="2"><a href="/api/logout">Logout</a></li>
+        ];
     }
   }
 
@@ -17,9 +25,12 @@ class Header extends Component {
     return (
       <nav>
         <div className="nav-wrapper">
-          <a className="left brand-logo">
-            feedbackLoop
-          </a>
+          <Link
+            to={this.props.auth ? '/surveys' : '/'}
+            className="left brand-logo"
+          >
+            FeedbackLoop
+          </Link>
           <ul className="right">
             {this.renderContent()}
           </ul>
